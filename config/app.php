bbdcbada6a91053663ebd92287ae0335f0aad7aa
Iwan Firmawan <firmawaneiwan@gmail.com>
2019-07-24 21:40:00 +0700

    add depedencies excel and tcpdf        4 => 'Dibatalkan',

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Elibyy\TCPDF\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -225,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'PDF' => Elibyy\TCPDF\Facades\TCPDF::class,
     ],
 
 ];
